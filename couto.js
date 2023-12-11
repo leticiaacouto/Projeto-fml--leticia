d0d26f8143ce083431d14d2cb515c8fdc5c8c86f
Letícia Couto <147053310+leticiaacouto@users.noreply.github.com>
2023-12-11 09:39:33 -0300

    Add files via upload

---
+++ couto.js
@@ -0,0 +1,26 @@
+//   inicio da galeria de imagens
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenesLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light');
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    
+    })
+})
+
+contenedorLight.addEventListener('click', (e) =>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.remove('show');
+        imagenesLight.classList.remove('showImage'); 
+    }
+})
+
+const aparecerImagen = (imagen) =>{
+    imagenesLight.src = imagen;
+    contenedorLight.classList.add('show');
+    imagenesLight.classList.add('showImage');
+}
+//   fim da galeria de imagens
+
